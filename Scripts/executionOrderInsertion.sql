882e34f08fd009332a6923ae6d7bdaa7eb44866e
Jose Storani <jdstorani91@gmail.com>
2023-08-09 15:59:04 -0300

    entrega insert

---
+++ Scripts/executionOrderInsertion.sql
@@ -0,0 +1,23 @@
+select * from branch;
+
+select * from employee;
+
+select * from fiscal_condition;
+
+select * from location;
+
+select * from client;
+
+select * from brand;
+
+select * from category;
+
+select * from products;
+
+select * from payment_method;
+
+select * from invoice;
+
+select * from invoice_products_detail;
+
+select * from products_stored;
